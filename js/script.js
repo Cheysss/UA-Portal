cf6672200c726f4d255a0010e6e8b0028bb7aa83
Cheysss <kenjisual07@gmail.com>
2024-04-07 16:34:45 +0800

    UA-1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function togglepopupSchedule() {
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function togglepopupBalance() {
+    document.getElementById("popup-2").classList.toggle("active");
+}
+
+function togglepopupInstructor() {
+    document.getElementById("popup-3").classList.toggle("active");
+}
+
+function togglepopupInformation() {
+    document.getElementById("popup-4").classList.toggle("active");
+}
